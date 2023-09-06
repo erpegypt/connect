d7c1c409c08c2aa970a1ce4d5a0e6f4244698e89
HagarMossad <123870763+HagarMossad@users.noreply.github.com>
2023-09-06 11:46:28 -0700

    Update total_stock_value.js

--- connect/connect/report/total_stock_value/total_stock_value.js
+++ connect/connect/report/total_stock_value/total_stock_value.js
@@ -3,6 +3,32 @@
 /* eslint-disable */
 
 frappe.query_reports["Total Stock Value"] = {
-	"filters": [		
+	"filters": [	
+		{
+			"fieldname":"group_by",
+			"label": __("Group By"),
+			"fieldtype": "Select",
+			"width": "80",
+			"reqd": 1,
+			"options": ["Warehouse", "Company"],
+			"default": "Warehouse",
+		},
+		{
+			"fieldname": "company",
+			"label": __("Company"),
+			"fieldtype": "Link",
+			"width": "80",
+			"options": "Company",
+			"reqd": 1,
+			"default": frappe.defaults.get_user_default("Company"),
+			"depends_on": "eval: doc.group_by != 'Company'",
+		},
+		{
+			"fieldname": "price_list",
+			"label": __("Price List"),
+			"fieldtype": "Link",
+			"width": "80",
+			"options": "Price List",
+		}
 	]
 };
